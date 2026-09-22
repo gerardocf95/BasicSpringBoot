699e7f0939a5cced94b451bf855d7f15d20097c0
gerardocf95 <gerardocf.ipn@gmail.com>
2026-09-21 20:56:12 -0600

    changed ArrayList to ConcurrentHashMap2

--- src/main/java/com/jerrycf/BasicSpringBoot/repository/InMemoryProductRepository.java
+++ src/main/java/com/jerrycf/BasicSpringBoot/repository/InMemoryProductRepository.java
@@ -3,36 +3,39 @@ package com.jerrycf.BasicSpringBoot.repository;
 import com.jerrycf.BasicSpringBoot.model.entity.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class InMemoryProductRepository implements ProductRepository {
 
-    private final AtomicLong idSequence = new AtomicLong(0);
+    private final AtomicLong idSequence;
 
-    private final List<Product> products = new ArrayList<>(List.of(
-            new Product(idSequence.incrementAndGet(), "Keyboard", 450.0),
-            new Product(idSequence.incrementAndGet(), "Mouse", 250.0),
-            new Product(idSequence.incrementAndGet(), "Monitor", 3200.0)
-    ));
+    private final ConcurrentHashMap<Long, Product> productMap = new ConcurrentHashMap<>();
+
+    private InMemoryProductRepository() {
+        idSequence = new AtomicLong(0);
+        productMap.put(idSequence.get(), new Product(idSequence.getAndIncrement(), "Keyboard", 450.0));
+        productMap.put(idSequence.get(), new Product(idSequence.getAndIncrement(), "Mouse", 250.0));
+        productMap.put(idSequence.get(), new Product(idSequence.getAndIncrement(), "Monitor", 320.0));
+    }
 
     @Override
     public List<Product> findAll() {
-        return products;
+        return productMap.values().stream().toList();
     }
 
     @Override
     public Optional<Product> findById(Long id) {
-        return products.stream().filter(product -> product.getId().equals(id)).findFirst();
+        return Optional.ofNullable(productMap.get(id));
     }
 
     @Override
     public Product save(Product product) {
-        product.setId(idSequence.incrementAndGet());
-        products.add(product);
+        product.setId(idSequence.get());
+        productMap.put(idSequence.getAndIncrement(), product);
         return product;
     }
 
